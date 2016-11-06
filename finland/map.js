ca53f1b6b93b15a82c7c120ec841219caa7fc3f5
rykovdenis <rykovdenis2000@gmail.com>
2016-11-06 21:13:04 +0300

    Add files via upload

---
+++ finland/map.js
@@ -0,0 +1,17 @@
+﻿        ymaps.ready(init);
+        var myMap, 
+            myPlacemark;
+        function init(){ 
+            myMap = new ymaps.Map("map", {
+                center: [60.17, 24.93],
+                zoom: 7
+            }); 
+            
+            myPlacemark = new ymaps.Placemark([60.17, 24.93], {
+                hintContent: '{Helsinki!',
+                balloonContent: 'Столица Финляндии'
+            });
+            
+            myMap.geoObjects.add(myPlacemark);
+        }
+  
